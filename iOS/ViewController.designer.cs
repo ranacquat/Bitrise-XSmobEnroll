dd36d9ac3803929275406ae90b39b061ec9b8589
ranacquat <ran.acquat@gmail.com>
2016-11-26 08:32:01 +0100

    update iOS entry windows

--- iOS/ViewController.designer.cs
+++ iOS/ViewController.designer.cs
@@ -1,23 +1,40 @@
-﻿//		
-// This file has been generated automatically by MonoDevelop to store outlets and		
-// actions made in the Xcode designer. If it is removed, they will be lost.		
-// Manual changes to this file may not be handled correctly.		
-//		
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace IsItU.iOS
 {
-	[Register("ViewController")]
+	[Register ("ViewController")]
 	partial class ViewController
 	{
 		[Outlet]
 		UIKit.UIButton Button { get; set; }
 
-		void ReleaseDesignerOutlets()
+		[Outlet]
+		UIKit.UITextField nameField { get; set; }
+
+		[Outlet]
+		UIKit.UITextField surnameField { get; set; }
+		
+		void ReleaseDesignerOutlets ()
 		{
-			if (Button != null)
-			{
-				Button.Dispose();
+			if (surnameField != null) {
+				surnameField.Dispose ();
+				surnameField = null;
+			}
+
+			if (nameField != null) {
+				nameField.Dispose ();
+				nameField = null;
+			}
+
+			if (Button != null) {
+				Button.Dispose ();
 				Button = null;
 			}
 		}
