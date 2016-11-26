834d83f056d16d888ac1fc0aab8680797df56b24
ranacquat <ran.acquat@gmail.com>
2016-11-26 09:19:34 +0100

    changing LaunchApp image

--- iOS/ViewController.designer.cs
+++ iOS/ViewController.designer.cs
@@ -1,42 +1,45 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
 
 namespace IsItU.iOS
 {
-	[Register ("ViewController")]
-	partial class ViewController
-	{
-		[Outlet]
-		UIKit.UIButton Button { get; set; }
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        UIKit.UIButton Button { get; set; }
 
-		[Outlet]
-		UIKit.UITextField nameField { get; set; }
 
-		[Outlet]
-		UIKit.UITextField surnameField { get; set; }
-		
-		void ReleaseDesignerOutlets ()
-		{
-			if (surnameField != null) {
-				surnameField.Dispose ();
-				surnameField = null;
-			}
+        [Outlet]
+        UIKit.UITextField nameField { get; set; }
 
-			if (nameField != null) {
-				nameField.Dispose ();
-				nameField = null;
-			}
 
-			if (Button != null) {
-				Button.Dispose ();
-				Button = null;
-			}
-		}
-	}
+        [Outlet]
+        UIKit.UITextField surnameField { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (Button != null) {
+                Button.Dispose ();
+                Button = null;
+            }
+
+            if (nameField != null) {
+                nameField.Dispose ();
+                nameField = null;
+            }
+
+            if (surnameField != null) {
+                surnameField.Dispose ();
+                surnameField = null;
+            }
+        }
+    }
 }
